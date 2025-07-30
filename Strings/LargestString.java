a92c74c53097423cab35197ba7dfd9fb53cd8c51
Nandni-Dixit <nandnidixit60@gmail.com>
2025-07-30 12:41:17 +0530

    Find the largest string in the group of string

---
+++ Strings/LargestString.java
@@ -0,0 +1,17 @@
+package DSA.Strings;
+
+public class LargestString {
+    public static void largestString(String[] fruits){
+        String largest = fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareToIgnoreCase(fruits[i])<0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    public static void main(String[] args) {
+        String[] fruits = {"Apple","Banana","Guava","Papaya","Strawberry","Muskmelon"};
+        largestString(fruits);
+    }
+}
